5ebaa3232adffe6316fe24d30ca92ffbb2c5b604
nachtm <nachtm@carleton.edu>
2018-01-27 22:36:52 -0600

    working api????

--- eve/settings.py
+++ eve/settings.py
@@ -1,8 +1,31 @@
+#This assumes that there is a database called 'test_courses' 
+#and that db has a table 'courses'
+
 #Mongodb settings
 MONGO_HOST = 'localhost'
-MONGO_PORT = 27107
+MONGO_PORT = 27017
 
-MONGO_DBNAME = 'classes'
+MONGO_DBNAME = 'test_courses'
+RENDERERS = ['eve.render.JSONRenderer']
 
 #definition of the API
-DOMAIN = {'people': {}}
+schema = {
+	'name': {
+		'type':'string',
+	},
+	'description': {
+		'type':'string',
+	}
+}
+
+courses = {	
+	'item_title': 'course',
+	'additional_lookup': {
+		'url': 'regex("[\w+]")',
+        'field': 'name'
+    },
+	'schema' : schema
+}
+
+DOMAIN = {'courses': courses}
+
